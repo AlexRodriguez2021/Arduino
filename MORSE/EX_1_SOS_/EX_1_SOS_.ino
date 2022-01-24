cf4485722e4575755cc801160a809e3075302a8c
Alex Rodriguez Zafra <alex.rodriguez.2019@lacetania.cat>
2022-01-24 14:26:36 +0100

    Create EX_1_SOS_.ino

---
+++ MORSE/EX_1_SOS_/EX_1_SOS_.ino
@@ -0,0 +1,67 @@
+/******************************************************************************
+**                                                                           **
+**                              Morse                                        **
+**                                                                           **
+**                                                                           **
+******************************************************************************/
+
+//****** Includes *************************************************************
+
+//****** Variables ************************************************************
+const int pin = 8;   // pin for speaker
+
+int note = 1047;
+int velocitat = 10;
+
+//****** Setup ****************************************************************
+void setup()
+{
+}
+
+//****** Loop *****************************************************************
+void loop()
+{
+  //S   ** Exemple SOS **
+  punt();
+  punt();
+  punt();
+  //Espai entre lletres
+  espaiL();
+  //O
+  ratlla();
+  ratlla();
+  ratlla();
+  //Espai entre lletres
+  espaiL();
+  //S
+  punt();
+  punt();
+  punt();
+  //Espai entre paraules
+  espaiP();
+}
+
+//****** Funcions *************************************************************
+void punt()
+{
+  tone(pin, note, 1000/velocitat);
+  delay(1000/velocitat); 
+  delay(1000/velocitat); 
+}
+
+void ratlla()
+{
+  tone(pin, note, 3*(1000/velocitat));
+  delay(3*(1000/velocitat)); 
+  delay(1000/velocitat);  
+}
+
+void espaiL()  //Espai entre lletres
+{  
+  delay(2*(1000/velocitat));  
+}
+
+void espaiP()  //Espai entre paraules
+{   
+  delay(6*(1000/velocitat));  
+}
